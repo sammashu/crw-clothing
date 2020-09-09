e3e9c60499dc2195c119b291260be07fd71c399f
Alexson Sanon Pel <alexson514@gmail.com>
2020-09-09 11:26:56 -0400

    add stylr bouton google adjusting

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,9 +51,10 @@ class SignIn extends React.Component{
                         handleChange={this.handleChange}
                         label="password"
                         required/>
-
-                    <CustomButton type='submit'>Submit Form</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Submit Form</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                    </div>
                 </form>
             </div>
         )
